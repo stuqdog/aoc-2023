93b40a43095f319c2733b7408548234509b177af
Ethan Rodkin <ethan@viam.com>
2023-12-03 10:30:02 -0500

    days 1-3

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use ::aoc_2023::solutions;
+
+fn main() {
+    println!("Day one");
+    solutions::day_one::main();
+
+    println!("Day two");
+    solutions::day_two::main();
+
+    println!("Day three");
+    solutions::day_three::main();
+}

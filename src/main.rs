70195b6e9b94814a0e79c351cef2045e0a4eabef
Ethan Rodkin <ethan@viam.com>
2023-12-05 09:38:30 -0500

    days 4-5

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ fn main() {
     println!("Day one");
     solutions::day_one::main();
 
-    println!("Day two");
+    println!("\nDay two");
     solutions::day_two::main();
 
-    println!("Day three");
+    println!("\nDay three");
     solutions::day_three::main();
+
+    println!("\nDay four");
+    solutions::day_four::main();
+
+    println!("\nDay five");
+    solutions::day_five::main();
 }
